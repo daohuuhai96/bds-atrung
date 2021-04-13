2f718cfc9a62d08c93984532046c5edc2f6bb185
daohuuhai96@gmail.com <daohuuhai96@gmail.com>
2021-04-13 11:13:08 +0700

    up

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
-<template>
-  <div id="app">
+<template class="w-full">
+  <div id="app" class="m-auto">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -17,12 +17,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
